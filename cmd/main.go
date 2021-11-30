e4758ef4b3e56dc657460bdeaf24d728f8878a0e
Troy Dai <git@troydai.com>
2021-11-29 21:22:52 -0800

    style code

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-  "io"
-  "net/http"
+	"io"
 	"log"
+	"net/http"
 
 	"go.uber.org/zap"
 )
@@ -14,26 +14,26 @@ func main() {
 		log.Fatalf("fail to provision a logger: %s", err)
 	}
 
-  echoHandler := func(w http.ResponseWriter, req *http.Request) {
-    logger.Info("inbound", zap.String("path", req.URL.Path), zap.String("method", req.Method))
+	echoHandler := func(w http.ResponseWriter, req *http.Request) {
+		logger.Info("inbound", zap.String("path", req.URL.Path), zap.String("method", req.Method))
 
-    body, err := io.ReadAll(req.Body)
-    if err != nil {
-      logger.Error("failed to read request body", zap.Error(err))
-      w.WriteHeader(500)
-    }
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			logger.Error("failed to read request body", zap.Error(err))
+			w.WriteHeader(500)
+		}
 
-    w.Write(body)
-  }
+		w.Write(body)
+	}
 
-  http.HandleFunc("/echo", echoHandler)
+	http.HandleFunc("/echo", echoHandler)
 
 	logger.Info("starting a HTTP server ...")
 	defer func() {
 		logger.Info("stopping ...")
 	}()
 
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    logger.Error("server exit with error", zap.Error(err))
-  }
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		logger.Error("server exit with error", zap.Error(err))
+	}
 }
