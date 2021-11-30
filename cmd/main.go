b0d63dd39ea5791645a9bf3c59a4fbf36d885bdd
Troy Dai <git@troydai.com>
2021-11-29 21:19:54 -0800

    add echo handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+  "io"
+  "net/http"
 	"log"
 
 	"go.uber.org/zap"
@@ -12,9 +14,26 @@ func main() {
 		log.Fatalf("fail to provision a logger: %s", err)
 	}
 
+  echoHandler := func(w http.ResponseWriter, req *http.Request) {
+    logger.Info("inbound", zap.String("path", req.URL.Path), zap.String("method", req.Method))
+
+    body, err := io.ReadAll(req.Body)
+    if err != nil {
+      logger.Error("failed to read request body", zap.Error(err))
+      w.WriteHeader(500)
+    }
+
+    w.Write(body)
+  }
+
+  http.HandleFunc("/echo", echoHandler)
 
 	logger.Info("starting a HTTP server ...")
 	defer func() {
 		logger.Info("stopping ...")
 	}()
+
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    logger.Error("server exit with error", zap.Error(err))
+  }
 }
