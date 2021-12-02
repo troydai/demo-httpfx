108e8f96f5f3a15abde418819c9ed71c2252fcf0
Troy Dai <git@troydai.com>
2021-12-02 09:11:12 -0800

    introduce fx

--- cmd/main.go
+++ cmd/main.go
@@ -2,54 +2,48 @@ package main
 
 import (
 	"context"
-	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"time"
 
 	"github.com/troydai/demo-httpfx/database"
 	"github.com/troydai/demo-httpfx/logging"
 	"github.com/troydai/demo-httpfx/server"
+	"go.uber.org/fx"
 	"go.uber.org/zap"
 )
 
 func main() {
-	logger, err := logging.NewLogger()
-	if err != nil {
-		log.Fatalf("fail to provision a logger: %s", err)
-	}
-
-	dataSource := database.NewDataSource()
-
-	server := server.NewHTTPServer(logger, dataSource)
-	Wait(StartServer(server, logger), logger)
+	fx.New(
+		fx.Provide(
+			logging.NewLogger,
+			database.NewDataSource,
+			server.NewHTTPServer,
+		),
+		fx.Invoke(
+			StartServer,
+		),
+	).Run()
 }
 
-func StartServer(server *http.Server, logger *zap.Logger) <-chan error {
+func StartServer(lc fx.Lifecycle, server *http.Server, logger *zap.Logger) error {
 	done := make(chan error)
-	go func() {
-		done <- server.ListenAndServe()
-	}()
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			go func() {
+				done <- server.ListenAndServe()
+			}()
+			logger.Info("starting a HTTP server. ctrl+c to exist.")
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			logger.Info("stopping server.")
+			if err := server.Shutdown(ctx); err != nil {
+				return err
+			}
+			<-done
+			logger.Info("server terminated.")
+			return nil
+		},
+	})
 
-	logger.Info("starting a HTTP server. ctrl+c to exist.")
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-
-	go func() {
-		<-c
-		logger.Info("stopping server.")
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-		if err := server.Shutdown(ctx); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	return done
-}
-
-func Wait(done <-chan error, logger *zap.Logger) {
-	<-done
-	logger.Info("server terminated.")
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/troydai/demo-httpfx
 
 go 1.13
 
-require go.uber.org/zap v1.19.1
+require (
+	go.uber.org/fx v1.15.0
+	go.uber.org/zap v1.19.1
+)
